c55046696a12ddb26659e5645f55a3055a945fb1
Will Cusick <william.m.cusick@stonybrook.edu>
2016-02-28 17:45:48 -0500

    Added method and constructor declaration. Empty blocks only atm

--- hw2/decafparser.py
+++ hw2/decafparser.py
@@ -28,8 +28,10 @@ def p_class_body(p):
 	pass
 
 def p_class_body_decl(p):
-	'''class_body_decl : field_decl'''
-	pass # TODO: add method_decl and constructor_decl
+	'''class_body_decl : field_decl
+					   | method_decl
+					   | constructor_decl'''
+	pass
 
 def p_field_decl(p):
 	'''field_decl : modifier var_decl'''
@@ -68,6 +70,36 @@ def p_variables(p):
 def p_variable(p):
 	'''variable : ID'''
 	pass
+
+def p_method_decl(p):
+	'''method_decl : modifier type ID LPAREN formals RPAREN block
+				   | modifier VOID ID LPAREN formals RPAREN block'''
+	# type and VOID can not be abstracted into a return_type rule!
+	# This causes a S/R conflict where on seeing the ID,
+	# it does not know whether to reduce the return_type or shift the ID
+	# (for var_decl)
+	pass
+
+def p_constructor_decl(p):
+	'''constructor_decl : modifier ID LPAREN formals RPAREN block'''
+
+def p_formals(p):
+	'''formals : formal_param formal_param_comma
+			   | empty'''
+	pass
+
+def p_formal_param(p):
+	'''formal_param : type variable'''
+	pass
+
+def p_formal_param_comma(p):
+	'''formal_param_comma : COMMA formal_param formal_param_comma
+						  | empty'''
+	pass
+
+def p_block(p):
+	'''block : LBRACE RBRACE'''
+	pass #TODO: add statements and expressions
 
 def p_empty(p):
 	'empty :'
